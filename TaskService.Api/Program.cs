1deb80c1d68730c15dcafde0a00c175feeef0d58
Joviank <Joviak@hotmail.no>
2026-04-07 13:46:33 +0200

    first commit

---
+++ TaskService.Api/Program.cs
@@ -0,0 +1,22 @@
+using TaskServiceClass = TaskService.TaskService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<TaskServiceClass>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.MapHealthChecks("health");
+app.Urls.Add("http://0.0.0.0:80");
+app.Run();
